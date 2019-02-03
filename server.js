5253a8d0b02e894e2c08b5a4171ec5915f6dca3c
Stefan Buhrmester <buhrmi@gmail.com>
2019-02-04 03:39:57 +0900

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ var proxy = require('http-proxy-middleware')
 
 var app = express()
 
+// serve files from static folder
 app.use(express.static('static'))
-app.use('/', proxy({ target: 'https://cm.46mp.com:9999/poker.cm', changeOrigin: true, cookieDomainRewrite: '', cookiePathRewrite: '' }))
-app.listen(3000)
 
-// http://localhost:3000/api/foo/bar -> http://www.example.org/api/foo/bar
+// if its not in static folder, proxy everything to 46mp.com
+app.use('/', proxy({ target: 'https://cm.46mp.com:9999/poker.cm', changeOrigin: true, cookieDomainRewrite: '', cookiePathRewrite: '' }))
+
+app.listen(3000)
